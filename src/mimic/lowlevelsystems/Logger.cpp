3ce7c85e9f82467cdcfa42c07ec9e14e299a864f
Ryan Bolson (s5326665) <cammellryan@gmail.com>
2023-10-31 13:56:48 +0000

    Created InternalLog function.

--- src/mimic/lowlevelsystems/Logger.cpp
+++ src/mimic/lowlevelsystems/Logger.cpp
@@ -6,30 +6,47 @@ namespace Mimic
 {
 	std::shared_ptr<Logger> Logger::_coreLogger;
 
+	void Logger::NewLine()
+	{
+		_vfprintf_l(stdout, "\n", nullptr, nullptr);
+	}
+
 	void Logger::Init()
 	{
 		_coreLogger = std::make_shared<Logger>();
 	}
 
-	const int Logger::LogWarning(char const* const format, ...)
+	const int Logger::DebugLog(char const* const format, ...)
 	{
-		Log("\033[33m");
-		Log("\n[Warning]: ");
 		int result;
 		va_list argList;
 		__crt_va_start(argList, format);
-		result = _vfprintf_l(stdout, format, NULL, argList);
+		result = _vfprintf_l(stdout, format, nullptr, argList);
 		__crt_va_end(argList);
-		Log("\033[0;37m");
+		NewLine();
 		return result;
 	}
 
-	const int Logger::Log(char const* const format, ...)
+	const int Logger::LogWarning(char const* const format, ...)
+	{
+		LogInternal("\033[33m");
+		LogInternal("[Warning]: ");
+		int result;
+		va_list argList;
+		__crt_va_start(argList, format);
+		result = _vfprintf_l(stdout, format, nullptr, argList);
+		__crt_va_end(argList);
+		LogInternal("\033[0;37m");
+		NewLine();
+		return result;
+	}
+
+	const int Logger::LogInternal(char const* const format, ...)
 	{
 		int result;
 		va_list argList;
 		__crt_va_start(argList, format);
-		result = _vfprintf_l(stdout, format, NULL, argList);
+		result = _vfprintf_l(stdout, format, nullptr, argList);
 		__crt_va_end(argList);
 		return result;
 	}
--- src/mimic/lowlevelsystems/Logger.h
+++ src/mimic/lowlevelsystems/Logger.h
@@ -4,8 +4,9 @@
 
 // Source: https://www.youtube.com/watch?v=dZr-53LAlOw&t=1619s
 
-#define MIMIC_LOG(...)				::Mimic::Logger::GetCoreLogger()->Log(__VA_ARGS__)
+#define MIMIC_LOG(...)				::Mimic::Logger::GetCoreLogger()->DebugLog(__VA_ARGS__)
 #define MIMIC_LOG_WARNING(...)		::Mimic::Logger::GetCoreLogger()->LogWarning(__VA_ARGS__)
+#define MIMIC_LOG_EROOR(...)		::Mimic::Logger::GetCoreLogger()->LogWarning(__VA_ARGS__)
 
 namespace Mimic
 {
@@ -15,11 +16,14 @@ namespace Mimic
 	struct Logger
 	{
 		static void Init();
+		const int LogError(char const* const format, ...);
 		const int LogWarning(char const* const format, ...);
-		const int Log(char const* const format, ...);
+		const int DebugLog(char const* const format, ...);
+		void NewLine();
 		inline static std::shared_ptr<Logger>& GetCoreLogger() { return _coreLogger; }
 
 	private:
+		const int LogInternal(char const* const format, ...);
 		static std::shared_ptr<Logger> _coreLogger;
 	};
 }
