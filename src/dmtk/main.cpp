90b8b8b5b9ef8115f811c67c39db02809e79a102
Ryan <cammellryan@gmail.com>
2023-12-03 17:15:13 +0000

    Testing shadow maps.

--- src/dmtk/main.cpp
+++ src/dmtk/main.cpp
@@ -28,7 +28,6 @@ using namespace MimicRender;
 #undef main
 int main(int argc, char* argv[])
 {
-	MIMIC_PROFILE_BEGIN_SESSION("Mimic Profile", "MimicProfile.json");
 	{
 		// create file loader:
 		std::shared_ptr<FileLoader> fileLoader = FileLoader::Initialise();
@@ -41,15 +40,15 @@ int main(int argc, char* argv[])
 		std::shared_ptr<Renderer> renderer = Renderer::Initialise();
 
 		// initialise shadow mapper:
-		std::shared_ptr<ShadowMapper> shadowMapper = ShadowMapper::Initialise(glm::ivec2(2048, 2048));
+		// std::shared_ptr<ShadowMapper> shadowMapper = ShadowMapper::Initialise(glm::ivec2(1024, 1024));
 
 		// initialise shaders:
 		const std::shared_ptr<Shader> pbrShader = Shader::Initialise(fileLoader->LocateFileInDirectory(assetPath, "PBRShader.glsl"));
-		glm::vec3 albedo = glm::vec3(0.3f);
+		glm::vec3 albedo = glm::vec3(1.0f, 0.0f, 0.0f);
 		glm::vec3 emissive = glm::vec3(0.0f, 0.0f, 0.0f);
-		float metallic = 0.236f;
-		float roughness = 0.039f;
-		float ambientOcclusion = 0.307f;
+		float metallic = 0.4f;
+		float roughness = 0.42f;
+		float ambientOcclusion = 0.05f;
 		float alpha = 1.0f;
 
 		const std::shared_ptr<Shader> blinnPhongShader = Shader::Initialise(fileLoader->LocateFileInDirectory(assetPath, "BlinnPhongShader.glsl"));
@@ -85,30 +84,37 @@ int main(int argc, char* argv[])
 
 		// create camera:
 		std::shared_ptr<Camera> camera = Camera::Initialise(window->GetAspectRatio(), 45.0f);
-		camera->Position = glm::vec3(0.0, 1.355f, -9.180f);
-		camera->Orientation = glm::vec3(0.0, -0.756f, -3.0f);
+		camera->Position = glm::vec3(0.0, 2.6f, 7.6f);
+		camera->Orientation = glm::vec3(0.0, -1.133f, -3.0f);
 
 		// create models:
 		glm::vec3 rotation = glm::vec3(0.0f);
-		glm::vec3 position = glm::vec3(0.0f, 0.0f, -14.285f);
-		std::shared_ptr<Model> model = Model::Initialise(fileLoader->LocateFileInDirectory(assetPath, "sphere.obj"));
+		glm::vec3 position = glm::vec3(0.0f, 0.235f, -14.285f);
+		int modelCount = 10;
+		std::vector< std::shared_ptr<Model>> models;
+		for (size_t i = 0; i < modelCount; i++)
+		{
+			models.push_back(Model::Initialise(fileLoader->LocateFileInDirectory(assetPath, "sphere.obj")));
+		}
+
+		// std::shared_ptr<Model> model = Model::Initialise(fileLoader->LocateFileInDirectory(assetPath, "sphere.obj"));
 		/*std::shared_ptr<Model> model1 = Model::Initialise(fileLoader->LocateFileInDirectory(assetPath, "normal_rock_sphere.obj"));
 		std::shared_ptr<Model> model2 = Model::Initialise(fileLoader->LocateFileInDirectory(assetPath, "normal_rock_sphere.obj"));
 		std::shared_ptr<Model> model3 = Model::Initialise(fileLoader->LocateFileInDirectory(assetPath, "normal_rock_sphere.obj"));
 		std::shared_ptr<Model> ground = Model::Initialise(fileLoader->LocateFileInDirectory(assetPath, "cube.obj"));*/
-		std::shared_ptr<Model> wall1 = Model::Initialise(fileLoader->LocateFileInDirectory(assetPath, "cube.obj"));
-		glm::vec3 wallPos = glm::vec3(0.0f, -1.24f, -50.0f);
-		glm::vec3 wallRot = glm::vec3(0.0f);
-		glm::vec3 wallScale = glm::vec3(43.45f, -0.595f, 50.0f);
-		std::shared_ptr<Model> lightModel = Model::Initialise(fileLoader->LocateFileInDirectory(assetPath, "normal_rock_sphere.obj"));
+		// std::shared_ptr<Model> wall1 = Model::Initialise(fileLoader->LocateFileInDirectory(assetPath, "cube.obj"));
+		// glm::vec3 wallPos = glm::vec3(0.0f, -1.24f, -50.0f);
+		// glm::vec3 wallRot = glm::vec3(0.0f);
+		// glm::vec3 wallScale = glm::vec3(43.45f, -0.595f, 50.0f);
+		// std::shared_ptr<Model> lightModel = Model::Initialise(fileLoader->LocateFileInDirectory(assetPath, "normal_rock_sphere.obj"));
 
-		const std::vector<std::shared_ptr<Model>> sceneModels = { model, wall1/*, model1, model2, wall1*/ };
+		// const std::vector<std::shared_ptr<Model>> sceneModels = { model, wall1/*, model1, model2, wall1*/ };
 
 		// create textures:
-		std::shared_ptr<Texture> albedoTexture;
-		std::shared_ptr<Texture> normalTexture;
-		std::shared_ptr<Texture> roughnessTexture;
-		std::shared_ptr<Texture> metallicTexture;
+		std::shared_ptr<Texture> albedoTexture = Texture::Initialise(fileLoader->LocateFileInDirectory(assetPath, "rustediron2_basecolor.png"), window->GetAspectRatio(), Texture::MIMIC_2D_TEXTURE_PARAMS, TextureType::MIMIC_ALBEDO);
+		std::shared_ptr<Texture> normalTexture = Texture::Initialise(fileLoader->LocateFileInDirectory(assetPath, "rustediron2_normal.png"), window->GetAspectRatio(), Texture::MIMIC_2D_TEXTURE_PARAMS, TextureType::MIMIC_NORMAL);
+		std::shared_ptr<Texture> roughnessTexture = Texture::Initialise(fileLoader->LocateFileInDirectory(assetPath, "rustediron2_roughness.png"), window->GetAspectRatio(), Texture::MIMIC_2D_TEXTURE_PARAMS, TextureType::MIMIC_ROUGHNESS);
+		std::shared_ptr<Texture> metallicTexture = Texture::Initialise(fileLoader->LocateFileInDirectory(assetPath, "rustediron2_metallic.png"), window->GetAspectRatio(), Texture::MIMIC_2D_TEXTURE_PARAMS, TextureType::MIMIC_METALLIC);
 
 		std::shared_ptr<Texture> sphereNormalTexture = Texture::Initialise(fileLoader->LocateFileInDirectory(assetPath, "Metal046B_1K-PNG_NormalGL.png"), window->GetAspectRatio(), Texture::MIMIC_2D_TEXTURE_PARAMS, TextureType::MIMIC_NORMAL);
 		// std::shared_ptr<Texture> groundAlbedoTexture = Texture::Initialise(fileLoader->LocateFileInDirectory(assetPath, "Metal046B_1K-PNG_Metalness.png"), window->GetAspectRatio(), Texture::MIMIC_2D_TEXTURE_PARAMS, TextureType::MIMIC_ALBEDO);
@@ -116,7 +122,7 @@ int main(int argc, char* argv[])
 		// load lights:
 		std::vector<std::shared_ptr<DirectLight>> directLights =
 		{
-			DirectLight::Initialise(glm::vec3(2.72f, 0.926f, -5.0f), glm::vec3(-1.0f, -1.0f, -0.246f), glm::vec3(100.0f))
+			DirectLight::Initialise(glm::vec3(2.72f, -3.14f, -5.0f), glm::vec3(-1.0f, -0.704f, -0.714f), glm::vec3(1.0f))
 		};
 
 		std::vector<std::shared_ptr<PointLight>> pointLights =
@@ -125,7 +131,7 @@ int main(int argc, char* argv[])
 		};
 
 		// load hdr environment map:
-		std::shared_ptr<EnvironmentCubeMap> environmentCubeMap = EnvironmentCubeMap::Initialise("studio_small_09_4k.hdr", window->GetAspectRatio(), renderer);
+		std::shared_ptr<EnvironmentCubeMap> environmentCubeMap = EnvironmentCubeMap::Initialise("rural_asphalt_road_4k.hdr", window->GetAspectRatio(), renderer);
 
 		// render-shader lambdas:
 		std::function<void()> pbrOnDrawLamba = [&]()
@@ -196,7 +202,7 @@ int main(int argc, char* argv[])
 				const glm::vec4 colour = glm::vec4(directLights[i]->Colour.x, directLights[i]->Colour.y, directLights[i]->Colour.z, 1.0f);
 				pbrShader->SetVector4((currentLight + ".colour").c_str(), colour);
 				pbrShader->SetMat4(currentLightMatrix.c_str(), directLights[i]->GetLightMatrix());
-				pbrShader->SetTexture(currentShadowMap.c_str(), directLights[i]->GetDepthMapTextureId(), 7, Texture::MIMIC_2D_TEXTURE);
+				// pbrShader->SetTexture(currentShadowMap.c_str(), directLights[i]->GetDepthMapTextureId(), 7, Texture::MIMIC_2D_TEXTURE);
 			}
 			pbrShader->SetInt("u_DirectLightsCount", directLights.size());
 
@@ -220,15 +226,15 @@ int main(int argc, char* argv[])
 		{
 			albedo = glm::vec3(0.4f);
 			// albedoTexture = nullptr;
-			normalTexture = sphereNormalTexture;
+			// normalTexture = sphereNormalTexture;
 			pbrOnDrawLamba();
 		};
 
 		std::function<void()> wallPBROnDrawLamba = [&]()
 		{
-			albedo = glm::vec3(0.6f);
+			// albedo = glm::vec3(0.6f);
 			// albedoTexture = groundAlbedoTexture;
-			normalTexture = nullptr;
+			// normalTexture = nullptr;
 			pbrOnDrawLamba();
 		};
 
@@ -252,7 +258,7 @@ int main(int argc, char* argv[])
 				const glm::vec4 colour = glm::vec4(directLights[i]->Colour.x, directLights[i]->Colour.y, directLights[i]->Colour.z, 1.0f);
 				blinnPhongShader->SetVector4((currentLight + ".colour").c_str(), glm::normalize(colour));
 				blinnPhongShader->SetMat4(currentLightMatrix.c_str(), directLights[i]->GetLightMatrix());
-				blinnPhongShader->SetTexture(currentShadowMap.c_str(), directLights[i]->GetDepthMapTextureId(), 0, Texture::MIMIC_2D_TEXTURE);
+				// blinnPhongShader->SetTexture(currentShadowMap.c_str(), directLights[i]->GetDepthMapTextureId(), 0, Texture::MIMIC_2D_TEXTURE);
 			}
 			blinnPhongShader->SetInt("u_DirectLightsCount", directLights.size());
 		};
@@ -278,6 +284,7 @@ int main(int argc, char* argv[])
 		//game loop:
 		// #############################################################################
 		bool applicationRunning = true;
+		MIMIC_PROFILE_BEGIN_SESSION("Mimic Profile", "MimicProfile.json");
 		while (applicationRunning)
 		{
 			MIMIC_PROFILE_FUNCTION();
@@ -318,12 +325,30 @@ int main(int argc, char* argv[])
 				camera->Update();
 				{
 					MIMIC_PROFILE_SCOPE("Update Model Matrices");
-					model->UpdateModelMatrix(position, rotation, glm::vec3(1.0f));
+					float x = 0.0f;
+					float z = 0.0f;
+					int maxRow = 5;
+					int index = 0;
+					float increment = 2.0f;
+
+					for (auto model : models)
+					{
+						model->UpdateModelMatrix(glm::vec3(x, 0.0f, z), glm::vec3(0.0f), glm::vec3(1.0f));
+						index++;
+						x += increment;
+						if (index > maxRow)
+						{
+							x = 0.0f;
+							z += increment;
+							index = 0;
+						}
+					}
+					// model->UpdateModelMatrix(position, rotation, glm::vec3(1.0f));
 					// model1->UpdateModelMatrix(glm::vec3(-2.5f, 0.0f, -4.0f), rotation, glm::vec3(1.0f));
 					// model2->UpdateModelMatrix(glm::vec3(2.5f, 0.0f, -4.0f), rotation, glm::vec3(1.0f));
 					// model3->UpdateModelMatrix(glm::vec3(2.5f, 1.0f, -4.0f), rotation, glm::vec3(1.0f));
-					wall1->UpdateModelMatrix(wallPos, wallRot, wallScale);
-					lightModel->UpdateModelMatrix(directLights[0]->Position, rotation, glm::vec3(0.2f));
+					// wall1->UpdateModelMatrix(wallPos, wallRot, wallScale);
+					//lightModel->UpdateModelMatrix(directLights[0]->Position, rotation, glm::vec3(0.2f));
 				}
 				// #############################################################################
 				// render scene:
@@ -333,21 +358,25 @@ int main(int argc, char* argv[])
 					glClearColor(0.77f, 0.73f, 0.97f, 1.0f);
 					glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 				}
-				// update shadow maps:
-				{
-					MIMIC_PROFILE_SCOPE("Render Depth Maps");
-					shadowMapper->RenderDirectLightDepthMaps(sceneModels, directLights, renderer);
-				}
 
+				//// update shadow maps:
+				//{
+				//	MIMIC_PROFILE_SCOPE("Render Depth Maps");
+				//	shadowMapper->RenderDirectLightDepthMaps(models, directLights, renderer);
+				//}
 				// send meshes to renderer:
 				{
 					MIMIC_PROFILE_SCOPE("Add Scene Objects To Draw Queue");
-					model->QueMeshesToDraw(blinnPhongShader, sphereBPOnDrawLamba, renderer);
+					for (auto model : models)
+					{
+						model->QueMeshesToDraw(pbrShader, modelPBROnDrawLamba, renderer);
+					}
+					// model->QueMeshesToDraw(blinnPhongShader, sphereBPOnDrawLamba, renderer);
 					/*model1->QueMeshesToDraw(pbrShader, pbrOnDrawLamba, renderer);
 					model2->QueMeshesToDraw(blinnPhongShader, blinnPhongOnDrawLamba, renderer);*/
-					wall1->QueMeshesToDraw(blinnPhongShader, wallBPOnDrawLamba, renderer);
+					// wall1->QueMeshesToDraw(blinnPhongShader, wallBPOnDrawLamba, renderer);
 					// ground->QueMeshesToDraw(pbrShader, pbrOnDrawLamba, renderer);
-					lightModel->QueMeshesToDraw(flatColourShader, flatColourOnDrawLamba, renderer);
+					// lightModel->QueMeshesToDraw(flatColourShader, flatColourOnDrawLamba, renderer);
 				}
 				// draw:
 				{
@@ -422,23 +451,23 @@ int main(int argc, char* argv[])
 			ImGui::SliderFloat3("Orientation##c2", &(camera->Orientation[0]), -5.0f, 5.0f);
 			ImGui::End();
 
-			// model controls:
-			ImGui::Begin("Model");
-			ImGui::SliderFloat3("Position##m1", &(position[0]), -20.0f, 20.0f);
-			ImGui::SliderFloat3("Rotation##m2", &(rotation[0]), -5.0f, 5.0f);
-			ImGui::End();
+			//// model controls:
+			//ImGui::Begin("Model");
+			//ImGui::SliderFloat3("Position##m1", &(position[0]), -20.0f, 20.0f);
+			//ImGui::SliderFloat3("Rotation##m2", &(rotation[0]), -5.0f, 5.0f);
+			//ImGui::End();
 
-			ImGui::Begin("Wall");
-			ImGui::SliderFloat3("Position##w2", &(wallPos[0]), -50.0f, 50.0f);
-			ImGui::SliderFloat3("Rotation##w2", &(wallRot[0]), -50.0f, 50.0f);
-			ImGui::SliderFloat3("Scale##w3", &(wallScale[0]), 0.0f, 50.0f);
-			ImGui::End();
+			//ImGui::Begin("Wall");
+			//ImGui::SliderFloat3("Position##w2", &(wallPos[0]), -50.0f, 50.0f);
+			//ImGui::SliderFloat3("Rotation##w2", &(wallRot[0]), -50.0f, 50.0f);
+			//ImGui::SliderFloat3("Scale##w3", &(wallScale[0]), 0.0f, 50.0f);
+			//ImGui::End();
 
 			ImGui::Render();
 			ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
 			window->SwapWindow();
 		}
+		MIMIC_PROFILE_END_SESSION();
 	}
-	MIMIC_PROFILE_END_SESSION();
 }
